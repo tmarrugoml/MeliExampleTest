2ca44c52f25117f8b095181de60a95a7da2b11e1
tmarrugoml <taidy.marrugo@mercadolibre.com.co>
2021-11-03 18:08:41 -0500

    CreditCartTest For workshop

---
+++ app/src/test/java/com/example/meliexampletest/CreditCartTest.kt
@@ -0,0 +1,35 @@
+package com.example.meliexampletest
+
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+//For  CHO WorkShop
+class CreditCartTest {
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+    @Test
+    fun validateCVV() {
+
+
+    }
+
+    @Test
+    fun validateDigits() {
+
+
+    }
+    @Test
+    fun validateExpirationDate() {
+
+
+    }
+
+}
