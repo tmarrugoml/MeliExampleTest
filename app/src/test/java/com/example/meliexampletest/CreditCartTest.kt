14d20de9d3a2fbe2b2229cb4042db06d329020e9
Mariangela Salcedo <mariangela.salcedo@mercadolibre.com.co>
2021-11-05 11:09:10 -0500

    workshop commit

--- app/src/test/java/com/example/meliexampletest/CreditCartTest.kt
+++ app/src/test/java/com/example/meliexampletest/CreditCartTest.kt
@@ -1,9 +1,7 @@
 package com.example.meliexampletest
 
 import org.junit.After
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
-import org.junit.Assert.assertNotNull
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
@@ -52,8 +50,73 @@ class CreditCartTest {
        el formato: **** **** **** 1234 (siendo 1234 los ultimos dígitos de la tarjeta)
      **/
 
+    @Test
+    fun check_digits_size_correct() {
+
+        val result = creditCard.setDigits("1234567891234567")
+
+        assertTrue(result)
+        assertNotNull(creditCard.digits)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun check_digits_size_incorrect() {
+
+        val result = creditCard.setDigits("123456789")
+
+        assertFalse(result)
+        assertNull(creditCard.digits)
+    }
+
+    @Test(expected = ClassCastException::class)
+    fun check_digits_digit_not_numeric() {
+
+        val result = creditCard.setDigits("123456S1222AA451")
+
+        assertFalse(result)
+        assertNull(creditCard.digits)
+    }
+
+    @Test
+    fun check_digits_format_correct() {
+
+        val input = "1234567891234567"
+        val expected = "**** **** **** 4567"
+        val result = creditCard.setDigits(input)
+
+        assertTrue(result)
+        assertEquals(expected, creditCard.digits)
+    }
+
     /** PROBLEMA 2
      * Validar la fecha de expiracion con formato MM/YY
      * Validar la fecha sea mayor que la actual
      **/
+
+    @Test
+    fun check_expirationDate_correct() {
+
+        val result = creditCard.setExpirationDate("12/21")
+
+        assertTrue(result)
+        assertNotNull(creditCard.expirationDate)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun check_expirationDate_incorrect() {
+
+        val result = creditCard.setExpirationDate("11/21")
+
+        assertFalse(result)
+        assertNull(creditCard.expirationDate)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun check_expirationDate_format_incorrect() {
+
+        val result = creditCard.setExpirationDate("111/24")
+
+        assertFalse(result)
+        assertNull(creditCard.expirationDate)
+    }
 }
